09a8e66f349635d0d8ec1d058428f857057db0f4
cloud_guru <voidcloud23@gmail.com>
2024-08-30 21:47:14 +0300

    checking the the equation equates to zero

--- implementations/07_quadratic_credit.c
+++ implementations/07_quadratic_credit.c
@@ -4,8 +4,8 @@
 int main()
 {
     float a, b, c, x, q1, q2;
-    // solving a quadratic equation
-    //  prompt user for the values
+
+    // prompt user for the values
     printf("Enter the values of a, b, c :\n");
     scanf("%f %f %f", &a, &b, &c);
     printf("%f %f %f\n", a, b, c);
@@ -19,10 +19,26 @@ int main()
     float res1 = (-b + sqrtf(x));
     q1 = res1 / (2 * a);
     printf("The solution using the + operator is: %f\n", q1);
-
+    // a*0.4*0.4 + b*(0.4) + c = 0
     float res2 = (-b - sqrtf(x));
     q2 = res2 / (2 * a);
     printf("The solution using the - operator is: %f\n", q2);
+    
+    // check if all solutions equate to zero after calculations
+    int pos = (a * q1 * q1) + (b * q1) + c == 0;
+    printf("%d first answer equates to zero\n", pos);
+
+    int neg = (a * q2 * q2) + (b * q2) + c == 0;
+    printf("%d second answer equates to zero\n", neg);
+
+    if (pos && neg != 0)
+    {
+        printf("double check your solutions");
+    }
+    else
+    {
+        printf("the solutions are correct\n");
+    }
 
     return 0;
 }
