4372554e4dbfefbb629984eda014cdf5d46e34e0
cloud_guru <voidcloud23@gmail.com>
2024-09-24 22:28:28 +0300

    prints the value of the array

---
+++ implementations/16_array_address.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+    int array[10] = {1, 2, 4, 6, 8, 10, 12, 14, 16, 18};
+
+     printf("%d\n", array);//prints the pointer of first element of the array as an integer
+
+    return 0;
+}
