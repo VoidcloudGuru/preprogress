9dd514cf3029b03cd8d2ce539f9147b8b86d92e5
cloud_guru <voidcloud23@gmail.com>
2024-09-25 15:42:55 +0300

    Use of pointers and memory addresses of a variable

--- implementations/15_pointers.c
+++ implementations/15_pointers.c
@@ -6,9 +6,9 @@ int main()
     int *ptr = &variable;
 
     printf("The value of the variable:%d\n", variable); // prints the value assigned to the variable
-    printf("The value of the pointer to the int variable is:%d\n", ptr);
+    printf("The value of the pointer to the int variable is:%p\n", ptr);
     // prints the value of the pointer which is the memory address of the variable
-    printf("The memory address of the int variable is:%d\n", &variable); 
+    printf("The memory address of the int variable is:%p\n", &variable); 
     // prints the memory address of the variable
     printf("The value held at the memory location that the pointer is pointing to is: %d\n", *ptr);
     // prints out the value of the variable stored at *ptr which is a pointer to the int variable
