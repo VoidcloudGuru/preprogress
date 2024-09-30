4716c3cfe63d4edf1387e632f9f580875e648365
cloud_guru <voidcloud23@gmail.com>
2024-09-30 14:05:27 +0300

    Declaring and defining a function hello and called in main

---
+++ implementations/17_function_declaration.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+// function declaration
+void Hello(void);
+
+int main()
+{
+    Hello();
+    return 0;
+}
+
+// function definition
+void Hello(void)
+{
+    printf("Hello World\n");
+}
