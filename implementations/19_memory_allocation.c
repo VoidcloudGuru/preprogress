38ac07024e4efd49afc39e3b91f1c808c7f1349d
cloud_guru <voidcloud23@gmail.com>
2024-10-01 14:19:04 +0300

    Allocation of memory on the heap with malloc and printing error or success message after compiling

--- implementations/19_memory_allocation.c
+++ implementations/19_memory_allocation.c
@@ -6,6 +6,7 @@ int main()
 {
     char *ptr;
     ptr = (char *)malloc(ARRAY_SIZE * sizeof(char));
+    // array size is multiplied by the char since char is one and we already have a speicific size for our array
     if (ptr == NULL)
     {
         printf("Memory allocation failed\n");
